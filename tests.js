66b0c95c6a1b9250a36b29c51fad6e01846f2a11
Duncan Ritchie <duncan.ritchie@informationcatalyst.com>
2022-10-12 21:22:17 +0100

    Improved in-browser tests

--- tests.js
+++ tests.js
@@ -2,18 +2,110 @@ const buttonTest = document.getElementById('test');
 
 //// Data to use in tests:
 
-const tests = [];
+const tests = [
+	{
+		Input: {
+			"Lemma": "ā",
+			"PartOfSpeech": "Noun",
+			"Meanings": "letter Aa",
+			"indeclinable": true,
+		},
+		Expected: {
+			"unencliticized": ["ā"],
+			"ne": ["āne"],
+			"que": ["āque"],
+			"ve": ["āve"],
+		}
+	},
+	{
+		Input: {
+			"Lemma": "ab",
+			"PartOfSpeech": "Preposition",
+			"Meanings": "from; by; since",
+			"Forms": {
+				"unencliticized": ["ā", "ab", "abs"],
+				"ne": ["āne"],
+				"que": ["āque"],
+				"ve": ["āve"],
+			},
+		},
+		Expected: {
+			"unencliticized": ["ā", "ab", "abs"],
+			"ne": ["āne"],
+			"que": ["āque"],
+			"ve": ["āve"],
+		},
+	},
+	{
+		Input: {
+			"Lemma": "abarceō",
+			"PartOfSpeech": "Verb",
+			"Meanings": "keep away",
+			"Conjugation": 2,
+		},
+		Expected: {},
+	},
+	{
+		Input: {
+			"Lemma": "abavia",
+			"PartOfSpeech": "Noun",
+			"Meanings": "great-great-grandmother",
+			"Declension": 1,
+		},
+		Expected: {},
+	},
+	{
+		Input: {
+			"Lemma": "abavus",
+			"PartOfSpeech": "Noun",
+			"Meanings": "great-great-grandfather",
+			"Declension": 2,
+		},
+		Expected: {},
+	},
+	{
+		Input: {
+			"Lemma": "abdicō",
+			"PartOfSpeech": "Verb",
+			"Meanings": "deny; abdicate; renounce",
+			"Conjugation": 1,
+		},
+		Expected: {},
+	},
+	{
+		Input: {
+			"Lemma": "abdīcō",
+			"PartOfSpeech": "Verb",
+			"Meanings": "deny (an omen or right)",
+			"Conjugation": 3,
+		},
+		Expected: {},
+	},
+];
 
 //// Tests looping over the above arrays:
 
 const test = () => {
-	tests.forEach(test => {
-		const actual = convert(test.Lemma);
-		if (actual === test.Expected) {
-			console.log(`Yay! ${test.Lemma}) => ${actual}`);
+	tests.forEach(({Input, Expected}) => {
+		const actual = inflectFuncs[Input.PartOfSpeech](Input.Lemma);
+
+		const actualStringified = JSON.stringify(actual);
+		const inputStringified = JSON.stringify(Input);
+		const expectedStringified = JSON.stringify(Expected);
+
+		if (Object.keys(Expected) === 0) {
+			console.log(`Expected forms have not been defined for this test; ${Input.Lemma}) => ${actual}`);
+		}
+		else if (actualStringified === '{}') {
+			console.log(`Inflection function has not been defined for ${Input.PartOfSpeech}`);
+		}
+		else if (actualStringified === expectedStringified) {
+			console.log(`Yay! ${Input.Lemma}) => ${actualStringified}`);
 		}
 		else {
-			console.error(`(${test.Lemma}) should give ${test.Expected} but actually gives ${actual}`);
+			console.error({
+				Input, Expected, Actual: actual
+			});
 		}
 	})
 }
